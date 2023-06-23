be687905027b487384c4118160bd8da805790861
Aditya Saravana <aditya.saravana@icloud.com>
2023-06-23 00:07:28 -0700

    cleanup, still gotta fix that weird bug with the NSImage in appdelegate

--- Butler/Butler/ButlerApp.swift
+++ Butler/Butler/ButlerApp.swift
@@ -15,99 +15,60 @@ import KeyboardShortcuts
 struct ButlerApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject private var appState = AppState()
-    @State var windowSize = "Will add support for windowSize soon."
     
-    var windowWidth: CGFloat {
-        if windowSize == "small" {
-            return 350
-        } else if windowSize == "medium" {
-            return 600
-        } else if windowSize == "large" {
-            return 1000
-        } else {
-            return 600
-        }
-    }
-    
-    var windowHeight: CGFloat {
-        if windowSize == "small" {
-            return 450
-        } else if windowSize == "medium" {
-            return 750
-        } else if windowSize == "large" {
-            return 1200
-        } else {
-            return 750
-        }
-    }
     
     var body: some Scene {
-        //        MenuBarExtra(content: {
-        //            ContentView(windowSize: $windowSize).environmentObject(OpenAIConnector()).frame(width: windowWidth, height: windowHeight)
-        //        }, label: {
-        //            let image: NSImage = {
-        //                let ratio = $0.size.height / $0.size.width
-        //                $0.size.height = 18
-        //                $0.size.width = 18 / ratio
-        //                return $0
-        //            }(NSImage(named: "MenuBarIcon")!)
-        //
-        //            Image(nsImage: image)
-        //        }).menuBarExtraStyle(.window)
-        //    }
-//        WindowGroup {
-//            ContentView(windowSize: $windowSize).environmentObject(OpenAIConnector()).frame(width: windowWidth, height: windowHeight)
-//        }
         WindowGroup {}
     }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
     
-    //@NSApplicationMain
-    class AppDelegate: NSObject, NSApplicationDelegate {
-        
-        var popover: NSPopover!
-        var statusBarItem: NSStatusItem!
-        
-        func applicationDidFinishLaunching(_ aNotification: Notification) {
-            if let window = NSApplication.shared.windows.first {
-                    window.close()
-                }
-            // Create the SwiftUI view that provides the window contents.
-            let contentView = ContentView(windowSize: .constant("sm")).environmentObject(OpenAIConnector())
-            
-            // Create the popover
-            let popover = NSPopover()
-            popover.contentSize = NSSize(width: 400, height: 500)
-            popover.behavior = .transient
-            popover.contentViewController = NSHostingController(rootView: contentView)
-            self.popover = popover
-            
-            // Create the status item
-            self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
-            
-            if let button = self.statusBarItem.button {
-                button.image = NSImage(named: "MenuBarIcon")
-                button.action = #selector(togglePopover(_:))
-            }
+    var popover: NSPopover!
+    var statusBarItem: NSStatusItem!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        if let window = NSApplication.shared.windows.first {
+            window.close()
         }
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView().environmentObject(OpenAIConnector())
         
-        @objc func togglePopover(_ sender: AnyObject?) {
-            if let button = self.statusBarItem.button {
-                if self.popover.isShown {
-                    self.popover.performClose(sender)
-                } else {
-                    self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
-                    self.popover.contentViewController?.view.window?.becomeKey()
-                }
-            }
+        // Create the popover
+        let popover = NSPopover()
+        popover.contentSize = NSSize(width: 400, height: 500)
+        popover.behavior = .transient
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        self.popover = popover
+        
+        // Create the status item
+        self.statusBarItem = NSStatusBar.system.statusItem(withLength: CGFloat(NSStatusItem.variableLength))
+        
+        if let button = self.statusBarItem.button {
+            //                let url = URL(fileURLWithPath: Bundle.main.path(forResource: "icondark", ofType: "png")!)
+            button.image = NSImage(named: "MenuBrIcon")
+            button.action = #selector(togglePopover(_:))
         }
     }
     
-    @MainActor
-    final class AppState: ObservableObject {
-        init() {
-            KeyboardShortcuts.onKeyUp(for: .openButler) { [self] in
-                NSApp.activate(ignoringOtherApps: true)
+    @objc public func togglePopover(_ sender: AnyObject?) {
+        if let button = self.statusBarItem.button {
+            if self.popover.isShown {
+                self.popover.performClose(sender)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+                self.popover.contentViewController?.view.window?.becomeKey()
             }
         }
     }
 }
+
+@MainActor
+final class AppState: ObservableObject {
+    init() {
+        KeyboardShortcuts.onKeyUp(for: .openButler) { [self] in
+            NSApp.activate(ignoringOtherApps: true)
+        }
+    }
+}
+
