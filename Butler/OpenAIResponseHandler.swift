e80fcb67bcb1c641bb3489a3db8a653f31551911
Aditya Saravana <aditya.saravana@icloud.com>
2023-07-05 18:43:50 -0700

    file management

--- Butler/OpenAIResponseHandler.swift
+++ Butler/OpenAIResponseHandler.swift
@@ -27,6 +27,7 @@ struct OpenAIResponseHandler {
         let json = jsonString.data(using: .utf8)!
         
         let decoder = JSONDecoder()
+        
         do {
             let product = try decoder.decode(OpenAIErrorResponse.self, from: json)
             return product
@@ -65,8 +66,8 @@ struct OpenAIErrorResponse: Codable {
 }
 
 struct OpenAIError: Codable {
-    var message: String
-    var type: String
+    var message: String?
+    var type: String?
     var param: String?
-    var code: String
+    var code: String?
 }
