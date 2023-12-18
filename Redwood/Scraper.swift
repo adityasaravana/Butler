69b045966d71d399a04815be6e94a43ed471519d
adityasaravana <aditya.saravana@icloud.com>
2023-12-18 10:58:33 -0800

    scraper

--- Redwood/Scraper.swift
+++ Redwood/Scraper.swift
@@ -11,16 +11,32 @@ import SwiftSoup
 class Scraper {
     static let shared = Scraper()
     
-    func scrapeHTML(from urlString: String) {
-        if let url = URL(string: "https://www.hackingwithswift.com") {
+    func scrapeHTML(from urlString: String) -> String? {
+        if let url = URL(string: urlString) {
             do {
                 let contents = try String(contentsOf: url)
                 print(contents)
+                return contents
             } catch {
-                // contents could not be loaded
+                print("Scraper.scrapeHTML: caught")
+                return nil
             }
         } else {
-            // the URL was bad!
+            print("Scraper.scrapeHTML: Bad URL")
+            return nil
+        }
+    }
+    
+    func parseTextFromHTML(from contents: String) -> String? {
+        do {
+           let doc: Document = try SwiftSoup.parse(contents)
+           return try doc.text()
+        } catch Exception.Error(let type, let message) {
+            print(message)
+            return nil
+        } catch {
+            print("error")
+            return nil
         }
     }
 }
